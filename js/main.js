13825fda07d56b7d3c3072e204db1b69551b1183
kovacsav <mezbogyo@gmail.com>
2022-10-02 20:03:52 +0200

    sitebuilding

---
+++ js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+const menu = document.querySelector(".menubar");
+
+window.onscroll = function () {
+  if (
+    //document.body.scrollTop >= 20
+    document.documentElement.scrollTop >= 20
+  ) {
+    menu.classList.add("menubar-white-bg");
+  } else {
+    menu.classList.remove("menubar-white-bg");
+  }
+};
